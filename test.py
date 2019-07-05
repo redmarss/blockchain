f81fcfb311ea4c741ff2defbef536fbf4d818b8e
redmarss <31237613+redmarss@users.noreply.github.com>
2019-07-05 18:09:14 +0800

    hpcdc

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-import scrapy
+#conding:utf-8
 
-print(scrapy.version_info)
+import requests
+import re
+
+starturl = "http://www.heibanke.com/lesson/crawler_ex00/"
+
+num = 0
+
+r = requests.get(starturl)
+reg = re.compile('<h3>(.*?)</h3>')
+
+f = re.findall(reg,r.text)
+print(f)
